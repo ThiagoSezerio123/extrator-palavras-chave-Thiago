e1bee320f1bbe7da60ea75d51cc7c832d1e24040
ThiagoSezerio123 <sezerio.thiago@escola.pr.gov.br>
2025-10-21 09:41:41 -0300

    21/10

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavraschaves );
 
 function mostraPalavraschaves() {
-    alert("Fui clicado!");
+    const texto = document.querySelector("#entrada-de-texto").value;
 }
