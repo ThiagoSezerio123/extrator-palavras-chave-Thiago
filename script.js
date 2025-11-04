791fa2c741c904308f7e291a30c7b81ec86f74f0
ThiagoSezerio123 <sezerio.thiago@escola.pr.gov.br>
2025-11-04 10:20:41 -0300

    04/11

--- script.js
+++ script.js
@@ -54,5 +54,12 @@ function contaFrequencias(palavras) {
 
 function tiraPalavrasRuins(palavras){
     const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "era", "o", "a", "as", "os"]);
-    
+        const palavrasBoas = [];
+
+        for (let palavra of palavras){
+            if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2){
+                palavrasBoas.push(palavra);
+            }
+        }
+        return palavrasBoas;
 }
